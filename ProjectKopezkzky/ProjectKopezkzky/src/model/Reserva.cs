2d01507e54ee426d9f3188f618f7d0cec1b54a40
JonathanKopezky <jonathan.oliveira@kumulus.com.br>
2021-11-09 19:05:42 -0300

    Firt commit logica reserva

---
+++ ProjectKopezkzky/ProjectKopezkzky/src/model/Reserva.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectKopezkzky.src.model
+{
+    public class Reserva
+    {
+        public int id { get; set; }
+        public int quartoId { get; set; }
+        public int pagamentoId { get; set; }
+        public int quantDiaria { get; set; }
+        public int responsavelId { get; set; }
+        public DateTime dataChckIn { get; set; }
+        public DateTime dataCheckOut { get; set; }
+        public int totalAcompanhante { get; set; }
+        public DateTime dataReserva { get; set; }
+        public String status { get; set; } 
+    }
+}
